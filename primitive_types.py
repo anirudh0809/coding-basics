f3b871c967af15c6109dab82ad9c17382bae7ea6
anirudh0809 <anirudh4448@gmail.com>
2021-07-19 14:58:03 -0400

    add

--- primitive_types.py
+++ primitive_types.py
@@ -1,5 +1,8 @@
 import math
 
+## To calculate parity of the given integer. Parity:0 if number of 1's in bits is even, else 0.
+
+# Brute force - bitwise check
 
 def parity_calculator(x:int) -> int:
     score=0
@@ -9,3 +12,15 @@ def parity_calculator(x:int) -> int:
     return score
 
 print(parity_calculator(2^64))
+# complexity O(n), n is word size 
+
+# Approach 2: Drop lowest set bit 
+
+def parity_calculator2(x:int) -> int:
+    score =0 
+    while x:
+        score ^= 1
+        x &= x-1
+    return score 
+
+print(parity_calculator2(2^64))
