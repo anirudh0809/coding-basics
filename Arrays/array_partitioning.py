8b60386b618c837aec9226dd0a3b150d89fdab8c
anirudh0809 <anirudh4448@gmail.com>
2021-07-28 19:26:10 -0400

    .

--- Arrays/array_partitioning.py
+++ Arrays/array_partitioning.py
@@ -1,13 +1,17 @@
 from typing import List
 
 
+
+# Two pass approach
 def partition(index: int , X:List[int]) -> None:
     pivot = X[index]
+    # Pass 1
     for i in range(len(X)):
         for j in range(i+1,len(X)):
             if X[j] < pivot:
                 X[i],X[j] = X[j],X[i]
                 break
+    #Pass 2
     for i in reversed(range(len(X))):
         for j in reversed(range(i)):
             if X[i]>pivot:
