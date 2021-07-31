18c73bc2d80c665f104c9bde7b314787c99a1f7d
anirudh0809 <anirudh4448@gmail.com>
2021-07-31 15:59:44 -0400

    partition2

--- Arrays/array_partitioning.py
+++ Arrays/array_partitioning.py
@@ -20,3 +20,25 @@ def partition(index: int , X:List[int]) -> None:
 
 # print(partition(2,[0,1,2,0,2,1,1]))
 # result : [1, 0, 0, 1, 1, 2, 2]
+
+
+def partition2(pivot_index:int,X:List[int])-> None:
+    pivot = X[pivot_index]
+    smaller=0
+    for i in range(len(X)):
+        if X[i] < pivot_index:
+            X[i],X[smaller] = X[smaller],X[i]
+            smaller +=1
+
+    larger = len(X) -1
+    for i in reversed(range(len(X))):
+        if X[i] > pivot_index:
+            X[i],X[larger] = X[larger],X[i]
+            larger -= 1
+
+    return X
+
+print(partition2(1,[0,1,2,0,2,1,1]))
+# result : [1, 0, 0, 1, 1, 2, 2]
+
+
